006df4ba7f0d5b69a6c62c86354fa03e01019c3b
Lucija Krsticevic <lkrstice1@pmfst.hr>
2023-05-19 14:23:34 +0200

    Verzija aplikacije za objavu na internetu

---
+++ frontend/build/precache-manifest.33a16deb81167a36cb8eedf12b0b7c3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e3bd0ba2c1ab05f4939c47c36d74b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11e29ce798ce5678c0d5",
+    "url": "/static/css/main.fbd243a5.chunk.css"
+  },
+  {
+    "revision": "96059e08242265f70900",
+    "url": "/static/js/2.c632e2ae.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/static/js/2.c632e2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11e29ce798ce5678c0d5",
+    "url": "/static/js/main.63eca48f.chunk.js"
+  },
+  {
+    "revision": "b5942b035685b24b37e7",
+    "url": "/static/js/runtime-main.0a493ba8.js"
+  }
+]);
